37bb8c85cb5ad403d1425ba7fa5b3ad090cef8e7
Kamszooo <jebacPIS09>
2023-11-16 23:19:37 +0100

    Dodano mechanizm analizy gazometrii

--- asystent_diagnozy/lib/badania/gazometria.dart
+++ asystent_diagnozy/lib/badania/gazometria.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
-import 'gazometria_results.dart';
-
 import 'package:flutter/services.dart';
+
+import 'gazometria_results.dart';
 
 class Gazometria extends StatefulWidget {
   const Gazometria({Key? key, required this.patientId}) : super(key: key);
@@ -18,27 +18,47 @@ class _GazometriaState extends State<Gazometria> {
   final _formKey = GlobalKey<FormState>();
 
   var items = {};
+  var interprets = {}; // pobrane interpretacje z JSON
 
-  var results = {};
+  Map<String, Map<String, dynamic>> results = {};
+  var interpretations = []; // wybrane interpretacje dla klasyfikacji
+  String classification = "Podane wyniki badania nie są standardowe";
 
   Future<void> readJson() async {
-    final String response = await rootBundle.loadString('assets/gazometria.json');
+    final String response =
+        await rootBundle.loadString('assets/gazometria.json');
     final data = await json.decode(response);
     setState(() {
-      items = data;
+      items = data['norms'];
+      interprets = data['int'];
     });
   }
 
   @override
   Widget build(BuildContext context) {
     readJson();
+
+    // String? lowerbound;
+    // String? upperbound;
+
+    // lowerbound = 'm_low';
+    // upperbound = 'm_high';
+
+    // lowerbound = 'w_low';
+    // upperbound = 'w_high';
+
     return SingleChildScrollView(
       child: Column(
         children: [
-          BackButton(
-            onPressed: () {
-              Navigator.pop(context, widget.patientId);
-            },
+          Row(
+            children: [
+              BackButton(
+                onPressed: () {
+                  Navigator.pop(context, widget.patientId);
+                },
+              ),
+              Text("patientId: ${widget.patientId}")
+            ],
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -46,7 +66,7 @@ class _GazometriaState extends State<Gazometria> {
               color: const Color.fromARGB(255, 255, 255, 255),
               child: Form(
                 key: _formKey,
-                child: Column(
+                child: Wrap(
                   children: [
                     for (var entry in items.entries)
                       Padding(
@@ -55,9 +75,13 @@ class _GazometriaState extends State<Gazometria> {
                           width: 200,
                           child: TextFormField(
                             keyboardType: TextInputType.number,
-                            inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
                             decoration: InputDecoration(
-                                border: const OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                                border: const OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5))),
                                 labelText: entry.value['short'],
                                 suffixIcon: Padding(
                                   padding: const EdgeInsets.only(right: 5),
@@ -68,10 +92,9 @@ class _GazometriaState extends State<Gazometria> {
                                     ),
                                   ),
                                 ),
-                                suffixIconConstraints: const BoxConstraints(minWidth: 0, minHeight: 0),
+                                suffixIconConstraints: const BoxConstraints(
+                                    minWidth: 0, minHeight: 0),
                                 isDense: true),
-
-                            // initialValue: '0',
                             validator: (value) {
                               if (value == null || value.isEmpty) {
                                 return 'Podaj prawidłową wartość';
@@ -79,7 +102,27 @@ class _GazometriaState extends State<Gazometria> {
                               return null;
                             },
                             onSaved: (value) {
-                              results[entry.key] = double.parse(value!);
+                              Map<String, dynamic> entryMap = {};
+                              entryMap['short'] = entry.value['short'];
+                              entryMap['value'] = double.parse(value!);
+                              entryMap['lowerbound'] = entry.value["low"];
+                              entryMap['upperbound'] = entry.value["high"];
+                              entryMap['result'] = (double.parse(value) <
+                                      entry.value["low"])
+                                  ? 'lt'
+                                  : (double.parse(value) > entry.value["high"])
+                                      ? 'gt'
+                                      : 'eq';
+                              results[entryMap['short']] = entryMap;
+                              // if (double.parse(value) <
+                              //     entry.value["low"]) {
+                              //   interpretations
+                              //       .addAll(entry.value['deficit_int']);
+                              // } else if (double.parse(value) >
+                              //     entry.value["high"]) {
+                              //   interpretations
+                              //       .addAll(entry.value['surplus_int']);
+                              // }
                             },
                           ),
                         ),
@@ -93,10 +136,104 @@ class _GazometriaState extends State<Gazometria> {
             onPressed: () {
               if (_formKey.currentState!.validate()) {
                 _formKey.currentState!.save();
+//logika klasyfikacji i interpretacji
+                // (pH | PaCO₂ | HCO₃ - akt) + BE
+                //
+                var pH = results["pH"]?["result"];
+                var PaCO2 = results["PaCO₂"]?["result"];
+                var HCO3 = results["HCO₃ - akt"]?["result"];
+                var BE = results["BE"]?["result"];
+
+                if (pH == "gt") {
+                  if (PaCO2 == "gt") {
+                    if (HCO3 == "gt") {
+                      classification =
+                          "Zasadowica metaboliczna częściowo skompensowana";
+                    }
+                  } else if (PaCO2 == "eq") {
+                    if (HCO3 == "gt") {
+                      classification =
+                          "Zasadowica metaboliczna nieskompensowana";
+                    }
+                  } else if (PaCO2 == "lt") {
+                    if (HCO3 == "eq") {
+                      classification = "Zasadowica oddechowa nieskompensowana";
+                    } else if (HCO3 == "lt") {
+                      classification =
+                          "Zasadowica oddechowa częściowo skompensowana";
+                    }
+                  }
+                } else if (pH == "lt") {
+                  if (PaCO2 == "gt") {
+                    if (HCO3 == "eq") {
+                      classification = "Kwasica oddechowa nieskompensowana";
+                    } else if (HCO3 == "gt") {
+                      classification =
+                          "Kwasica oddechowa częściowo skompensowana";
+                    }
+                  } else if (PaCO2 == "eq") {
+                    if (HCO3 == "lt") {
+                      classification = "Kwasica metaboliczna nieskompensowana";
+                    }
+                  } else if (PaCO2 == "lt") {
+                    if (HCO3 == "lt") {
+                      classification =
+                          "Kwasica metaboliczna częściowo skompensowana";
+                    }
+                  }
+                } else if (pH == "eq") {
+                  if (PaCO2 == "gt" && HCO3 == "gt") {
+                    //kwasica oddechowa albo zasadowica metaboliczna
+                    if (BE == "eq") {
+                      // "Jeśli BE jest w normie -> mamy całkowicie skompensowane zaburzenie oddechowe"
+                      classification = "Kwasica oddechowa skompensowana";
+                    } else if (BE == "gt") {
+                      //"Jeśli nie jest -> mamy całkowicie skompensowane zaburzenie metaboliczne" + "Dodatnie BE wskazuje, że ilość zasad przekracza norme (zasadowica metaboliczna)"
+                      classification = "Zasadowica metaboliczna skompensowana";
+                    }
+                  } else if (PaCO2 == "lt" && HCO3 == "lt") {
+                    //zasadowica oddechowa albo kwasica metaboliczna
+                    if (BE == "eq") {
+                      // "Jeśli BE jest w normie -> mamy całkowicie skompensowane zaburzenie oddechowe"
+                      classification = "Zasadowica oddechowa skompensowana";
+                    } else if (BE == "gt") {
+                      //"Jeśli nie jest -> mamy całkowicie skompensowane zaburzenie metaboliczne" + "Dodatnie BE wskazuje, że ilość zasad przekracza norme (zasadowica metaboliczna)"
+                      classification = "Kwasica metaboliczna skompensowana";
+                    }
+                  }
+
+                  // if (results?["pH"]?["value"]?.value >= 7.4)
+                }
+                switch (classification) {
+                  case "Kwasica oddechowa nieskompensowana" ||
+                        "Kwasica oddechowa częściowo skompensowana" ||
+                        "Kwasica oddechowa skompensowana":
+                    interpretations = interprets["KO"];
+                    break;
+                  case "Zasadowica oddechowa nieskompensowana" ||
+                        "Zasadowica oddechowa częściowo skompensowana" ||
+                        "Zasadowica oddechowa skompensowana":
+                    interpretations = interprets["ZO"];
+                    break;
+                  case "Kwasica metaboliczna nieskompensowana" ||
+                        "Kwasica metaboliczna częściowo skompensowana" ||
+                        "Kwasica metaboliczna skompensowana":
+                    interpretations = interprets["KO"];
+                    break;
+                  case "Zasadowica metaboliczna nieskompensowana" ||
+                        "Zasadowica metaboliczna częściowo skompensowana" ||
+                        "Zasadowica metaboliczna skompensowana":
+                    interpretations = interprets["ZO"];
+                    break;
+                }
+
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => GazometriaAnaliza(results: results),
+                      builder: (context) => GazometriaAnaliza(
+                          results: results,
+                          interpretations: interpretations,
+                          clasification: classification),
                     ));
               }
             },
--- asystent_diagnozy/lib/badania/gazometria_results.dart
+++ asystent_diagnozy/lib/badania/gazometria_results.dart
@@ -1,81 +1,109 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/services.dart';
 
 class GazometriaAnaliza extends StatefulWidget {
-  const GazometriaAnaliza({
-    Key? key,
-    required this.results,
-  }) : super(key: key);
+  const GazometriaAnaliza(
+      {Key? key,
+      required this.results,
+      required this.interpretations,
+      required this.clasification})
+      : super(key: key);
 
-  final Map results;
+  final Map<String, Map<String, dynamic>> results;
+  final String clasification;
+  final List interpretations;
 
   @override
   State<GazometriaAnaliza> createState() => _GazometriaAnalizaState();
 }
 
 class _GazometriaAnalizaState extends State<GazometriaAnaliza> {
-  var items = {};
-
-  Future<void> readJson() async {
-    final String response = await rootBundle.loadString('assets/gazometria.json');
-    final data = await json.decode(response);
-    setState(() {
-      items = data;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    readJson();
     return SingleChildScrollView(
-      child: Column(
+      child: Row(
         children: [
-          TextButton(
-              onPressed: () {
-                Navigator.pop(context, widget.results);
-              },
-              child: const Text("Powrót")),
-          DataTable(
-            decoration: const BoxDecoration(color: Colors.white),
-            columns: const <DataColumn>[
-              DataColumn(
-                label: Text('Parametr'),
-              ),
-              DataColumn(
-                label: Text('Jednostka'),
-              ),
-              DataColumn(
-                label: Text('min'),
-              ),
-              DataColumn(
-                label: Text('max'),
-              ),
-              DataColumn(
-                label: Text('flag'),
-              ),
-            ],
-            rows: <DataRow>[
-              for (final entry in items.entries)
-                DataRow(
-                  cells: <DataCell>[
-                    DataCell(Text(entry.value['short'])),
-                    DataCell(Text("${widget.results[entry.key]} ${entry.value['unit']}")),
-                    DataCell(Text(entry.value['low'].toString())),
-                    DataCell(Text(entry.value['high'].toString())),
-                    DataCell(
-                      (widget.results[entry.key] < entry.value['low'])
-                          ? const Text('za malo')
-                          : (widget.results[entry.key] > entry.value['high'])
-                              ? const Text('za duzo')
-                              : const Text('w normie'),
-                    )
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context, widget.results);
+                    },
+                    child: const Text("Powrót")),
+                DataTable(
+                  border: const TableBorder(
+                      verticalInside: BorderSide(
+                          width: 2, color: Color.fromRGBO(238, 238, 238, 1))),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  columns: const <DataColumn>[
+                    DataColumn(
+                      label: Text('Parametr'),
+                    ),
+                    DataColumn(
+                      label: Text('Jednostka'),
+                    ),
+                    DataColumn(
+                      label: Text('min'),
+                    ),
+                    DataColumn(
+                      label: Text('max'),
+                    ),
+                    DataColumn(
+                      label: Text('flag'),
+                    ),
                   ],
-                )
-            ],
+                  rows: <DataRow>[
+                    for (final entry in widget.results.entries)
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(entry.value['short'])),
+                          DataCell(Text(
+                              "${entry.value['value']} ${entry.value['unit']}")),
+                          DataCell(Text(entry.value['lowerbound'].toString())),
+                          DataCell(Text(entry.value['upperbound'].toString())),
+                          DataCell(Text(entry.value['result']))
+                        ],
+                      )
+                  ],
+                ),
+              ],
+            ),
           ),
+          Expanded(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 147, 146, 202)),
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(widget.clasification),
+                    ),
+                  ),
+                ),
+                Wrap(direction: Axis.horizontal, children: [
+                  for (var entry in widget.interpretations)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            color: Colors.white),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(entry),
+                        ),
+                      ),
+                    )
+                ])
+              ],
+            ),
+          )
         ],
       ),
     );
