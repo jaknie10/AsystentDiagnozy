a211cbfbda19686a5ed7184e408ceddff5a79f0e
kuba <kuba@niemir.com>
2023-11-30 12:21:05 +0100

    Zmiany w UI - pasek górny

--- asystent_diagnozy/lib/layout.dart
+++ asystent_diagnozy/lib/layout.dart
@@ -29,8 +29,16 @@ class _LayoutState extends State<Layout> {
 
     List<Map<String, dynamic>> pages = [
       {'name': 'Home', 'page': const HomePage(), 'key': keyHome},
-      {'name': 'Profile', 'page': const Profile(doctorId: 1), 'key': keyProfile},
-      {'name': 'Settings', 'page': const Settings(doctorId: 1), 'key': keySettings},
+      {
+        'name': 'Profile',
+        'page': const Profile(doctorId: 1),
+        'key': keyProfile
+      },
+      {
+        'name': 'Settings',
+        'page': const Settings(doctorId: 1),
+        'key': keySettings
+      },
     ];
 
     return Scaffold(
@@ -53,51 +61,60 @@ class _LayoutState extends State<Layout> {
               onPressed: () {
                 changeState(0);
               },
-              icon: ImageIcon(
-                const AssetImage('assets/cardiologyLogo.png'),
-                size: 100,
-                color: Theme.of(context).colorScheme.primary,
-              ),
+              icon: ImageIcon(const AssetImage('assets/cardiologyLogo.png'),
+                  size: 100, color: Theme.of(context).colorScheme.primary),
             )),
         leadingWidth: 80,
         title: const Align(
-          alignment: Alignment.centerLeft,
-          child: Text("Asystent diagnozy", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30, letterSpacing: 1.5),)
-        ),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Asystent diagnozy",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  letterSpacing: 1.5),
+            )),
         actions: [
           Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  _selectedIndex = 1;
-                });
-              },
-              child: Container(
-                height: 80,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0), color: Theme.of(context).colorScheme.background,),
-                padding: const EdgeInsets.all(10.0),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                      SvgPicture.asset(
-                      'assets/lekarz_logo.svg',
-                      width: 45,
-                      fit: BoxFit.scaleDown,
+              padding: const EdgeInsets.all(10.0),
+              child: MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex = 1;
+                    });
+                  },
+                  child: Container(
+                    height: 80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      color: Theme.of(context).colorScheme.background,
                     ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15.0),
-                        child: Text("Jan Kowalski", style: TextStyle(fontSize: 20, color: Color.fromRGBO(22, 20, 35, 1.0))),
-                      ),
-                      IconButton(
-                        onPressed: (){
-                        }, 
-                        icon: Icon(Icons.arrow_drop_down, size: 25,), color: Color.fromRGBO(22, 20, 35, 1.0),)
-                    ])),
-              ),
-            ),
-          )
+                    padding: const EdgeInsets.all(10.0),
+                    child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              SvgPicture.asset(
+                                'assets/lekarz_logo.svg',
+                                width: 45,
+                                fit: BoxFit.scaleDown,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                                child: Text("Jan Kowalski",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color:
+                                            Color.fromRGBO(22, 20, 35, 1.0))),
+                              ),
+                            ])),
+                  ),
+                ),
+              ))
         ], // default is 56
       ),
       body: Row(
