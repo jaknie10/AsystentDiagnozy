2eba51c90006dd16015a2090a686d79047001cd5
jakgrz7 <91895268+jakgrz7@users.noreply.github.com>
2023-12-06 18:46:33 +0100

    Dodanie limitu zmniejszania okna aplikacji

--- asystent_diagnozy/lib/main.dart
+++ asystent_diagnozy/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'layout.dart';
+import 'package:window_size/window_size.dart';
+import 'dart:io';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Asystent diagnozy');
+    setWindowMinSize(const Size(1466, 768));
+    setWindowMaxSize(Size.infinite);
+  }
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,7 +31,8 @@ class MyApp extends StatelessWidget {
             background: const Color.fromRGBO(238, 238, 238, 1)),
         fontFamily: 'Montserrat',
         scrollbarTheme: const ScrollbarThemeData().copyWith(
-          thumbColor: const MaterialStatePropertyAll(Color.fromRGBO(0, 99, 248, 1)),
+          thumbColor:
+              const MaterialStatePropertyAll(Color.fromRGBO(0, 99, 248, 1)),
           thumbVisibility: const MaterialStatePropertyAll(true),
           trackVisibility: const MaterialStatePropertyAll(true),
         ),
