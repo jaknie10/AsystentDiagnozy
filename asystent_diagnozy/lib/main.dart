43de2570110f15b1e96858343a7cde76a26ec2ed
s464837 <jaknie10@st.amu.edu.pl>
2024-01-04 23:59:53 +0100

    zmiana rozdzielczości

--- asystent_diagnozy/lib/main.dart
+++ asystent_diagnozy/lib/main.dart
@@ -9,7 +9,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     setWindowTitle('Asystent diagnozy');
-    setWindowMinSize(const Size(1366, 768));
+    setWindowMinSize(const Size(1280, 720));
     setWindowMaxSize(Size.infinite);
   }
 
